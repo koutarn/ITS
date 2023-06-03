4692a31e62557708d969051ee7ce09dd28d2e422
koutarn <fuse.kanrinin@gmail.com>
2023-06-04 01:14:57 +0900

    O/RマッパーのRepositoryクラスの追加

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO issues (summary, description) VALUES ('bug1', 'aaaa');
+INSERT INTO issues (summary, description) VALUES ('bug2', 'bbbb');
+INSERT INTO issues (summary, description) VALUES ('bug3', 'cccc');
